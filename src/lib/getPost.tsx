9cb9d22e0f35c6856edf24c8e04ab8c6e3a2e7f8
Lucas Lauriano de Souza <lucaslauriano.souza@gmail.com>
2023-10-21 18:21:03 -0300

    feat: implementing app store to store all posts

---
+++ src/lib/getPost.tsx
@@ -0,0 +1,20 @@
+import { IJob, IResponse } from '@/types/posts';
+
+const baseUrl = process.env.REACT_APP_SERVICE_BASE_URL as RequestInfo | URL;
+export const findById = (data: IResponse[], id: number) => {
+  for (let i = 0; i < data.length; i++) {
+    const el: IResponse = data[i];
+
+    if (el.id == id) {
+      return el;
+    }
+  }
+  return null;
+};
+export async function getPost(postId: number): Promise<IJob | null> {
+  const res = await fetch(baseUrl);
+
+  if (!res.ok) throw new Error('Failed to fetch post');
+  const data = await res.json();
+  return findById(data, postId);
+}
---
+++ src/stores/useAppStore.ts
@@ -0,0 +1,19 @@
+import { StateCreator, create } from 'zustand';
+
+import { getAllposts } from '@/lib/getAllPosts';
+
+export interface PostsStores {
+  posts: any[];
+  fetchPosts: () => void;
+}
+export const createPostsSlice: StateCreator<PostsStores> = (set) => ({
+  posts: [] as any[],
+  fetchPosts: async () => {
+    const posts = await getAllposts();
+    set({ posts });
+  },
+});
+
+export const useAppStore = create<PostsStores>()((...a) => ({
+  ...createPostsSlice(...a),
+}));
